523b00fb91e608829f1824ba96c534b4cd29874a
WillerLucoles <willerlob@gmail.com>
2025-01-06 19:49:26 -0300

    chore: configure Tailwind CSS and Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 
-import './App.css'
-
 function App() {
 
   return (
     <>
       <main>
-        <h1>Seja bem vindo! :)</h1>
+        <h1 className='text-black font-bold'>Seja bem vindo! :)</h1>
         <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
       </main>
     </>
