95aff8b36f025e6d1022cd537549b5dbc8ae9e8e
WillerLucoles <willerlob@gmail.com>
2025-01-06 22:11:46 -0300

    fix: current year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import SocialLinks from "./SocialLinks"
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className='w-full mt-10 md:m-0 md:pt-10'>
       <section className='px-5 md:px-10 py-6 mx-auto'>
@@ -15,7 +16,7 @@ const Footer = () => {
             ))}
           </ul>
         </nav>
-        <p className='text-sm text-center text-black'>&copy; 2025 Willer Lucoles. Feito com <span className="invert">🤍</span> em Minas Gerais, Brasil.</p>
+        <p className='text-sm text-center text-black'>&copy; {currentYear} Willer Lucoles. Feito com <span className="invert">🤍</span> em Minas Gerais, Brasil.</p>
       </section>
     </footer>
   )
